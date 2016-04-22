891881a23ff43940a519e3605dcd0deba81aad81
michaelrhodes <spam@michaelrhod.es>
2016-04-21 19:40:16 -0700

    Fix formatting

--- supports.js
+++ supports.js
@@ -8,7 +8,7 @@ module.exports = function (algo, cb) {
   // Browsers throw if they lack support for an algorithm.
   // Promise will be rejected on non-secure origins. (http://goo.gl/lq4gCo)
   try { subtle.digest({ name: normal(algo) }, buf).then(yep, nope) }
-  catch (err) {setTimeout(nope, 0, err) }
+  catch (err) { setTimeout(nope, 0, err) }
 
   function yep () { cb(true) }
   function nope (err) { cb(false, err) }
